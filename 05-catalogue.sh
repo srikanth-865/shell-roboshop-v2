f720e9b5f077d29f734cd80af24901afb17d5802
srikanth-865 <srikanthram87@gmail.com>
2026-06-13 08:55:06 +0530

    srikanth

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -1,40 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 
+app_name=catalogue
 source ./common.sh
 check_root
 
-dnf module disable nodejs -y &>>$LOGS_FILE
-dnf module enable nodejs:20 -y  &>>$LOGS_FILE
-dnf install nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Installing NodeJS:20"
-
-id roboshop &>>$LOGS_FILE
-if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
-fi
-
-rm -rf /app
-VALIDATE $? "Removing existing code"
-
-rm -rf /tmp/catalogue.zip
-VALIDATE $? "Removed catalogue zip"
-
-mkdir -p /app  &>>$LOGS_FILE
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOGS_FILE
-VALIDATE $? "Downloaded and extracted catalogue code"
-
-npm install  &>>$LOGS_FILE
-VALIDATE $? "Installing dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Created systemctl service"
+app_setup
+nodejs_setup
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Added Mongo repo" 
@@ -42,15 +14,14 @@ VALIDATE $? "Added Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host mongodb.srikanth865.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.daws90s.shop --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -lt 0 ]; then
-    mongosh --host mongodb.srikanth865.online </app/db/master-data.js &>>$LOGS_FILE
+    mongosh --host mongodb.daws90s.shop </app/db/master-data.js &>>$LOGS_FILE
     VALIDATE $? "Load Products"
 else
     echo -e "Products already loaded ... $Y SKIPPING $N"
 fi
 
-systemctl enable catalogue &>>$LOGS_FILE
-systemctl restart catalogue &>>$LOGS_FILE
-VALIDATE $? "Restarting catalogue"
+app_restart
+print_total_time
